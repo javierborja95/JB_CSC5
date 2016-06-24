26686bb77de5695ce57b3f8bbe9f0fe1ed106170
javierborja95 <javierborja95@gmail.com>
2016-06-23 18:31:46 -0700

    template

---
+++ Class/Template/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on June , 2016,  PM
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+using namespace std;  //iostream uses the standard namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+ 
+    //Input Data
+       
+    //Process Data
+                   
+    //Output Data
+ 
+    //Exit Stage Right!
+    return 0;
+}
+
