c444d3bb157fceb894e533a28e9985553f45e6fc
javierborja95 <javierborja95@gmail.com>
2016-06-27 11:35:52 -0700

    labwork

---
+++ Lab/TruthTable/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on June , 2016,  PM
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+
+using namespace std;  //iostream uses the standard namespace
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+cout.setf(ios::fixed);
+cout.setf(ios::showpoint);
+cout.precision();
+    //Variables
+ 
+    //Input Data
+
+    //Process Data
+                   
+    //Output Data
+
+    return 0;
+}
+
