911bd86de67ea4722f6b4c83489057da95f98111
javierborja95 <javierborja95@gmail.com>
2016-06-28 20:31:42 -0700

    hw

---
+++ Hmwk/Assignment_2/Assignment_2_menu/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on June , 2016,  PM
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+using namespace std;  //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+cout.setf(ios::fixed);
+cout.setf(ios::showpoint);
+cout.precision();
+    //Variables
+ 
+    //Input Data
+
+    //Process Data
+                   
+    //Output Data
+
+    return 0;
+}
+
