9c6871b050b7f9e01cdce59ae5386dd15104cd9d
javierborja95 <javierborja95@gmail.com>
2016-07-06 11:40:34 -0700

    class

---
+++ Class/Retirement/main.cpp
@@ -0,0 +1,58 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on June 6, 2016, 11:00 PM
+ * Purpose: Retirement 
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+#include <iomanip>
+using namespace std;  //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+    //Variables
+    float pv, fv, invRate, depRate, deposit, salary, retSav;
+    
+    //Input Data
+    cout<<"Input Salary in $'s, the deposit %, interest rate in %, "
+          <<endl;
+    cin>>salary>>depRate>>invRate;
+    
+    //Process Data
+    invRate/=100.0f;  //Convert to decimal 
+    depRate/=100.0f;//Convert to decimal
+    deposit=depRate*salary;
+    retSav=salary/invRate;
+    fv=pv=0;        //Initialize the future value with the present
+    
+    //Loop to Display the yearly result
+    cout<<fixed;
+    cout<<"Our salary =            $"<<setprecision(2)<<salary<<endl;
+    cout<<"Yearly Bond Purchase = $"<<setprecision(2)<<deposit<<endl;
+    cout<<"Savings at Retirement = $"<<setprecision(2)<<retSav<<endl;
+    cout<<"Table Produced with Investment Rate = "<<setprecision(2)
+         <<invRate*100<<"% interest\n";
+    cout<<"Year    Date     Savings $\n";
+    int year=0, dateYr=2016;
+    do{
+        cout<<setw(4)<<year<<setw(8)<<dateYr<<setw(12)<<setprecision(2)<<fv<<endl;
+        fv*=(1+invRate);//pay each year some interest
+        fv+=deposit;
+        int ifv=fv*100;
+        fv=ifv/100.0f;//Truncating to nearest penny
+        year++;
+        dateYr++;
+    }while(fv<retSav);
+        cout<<setw(4)<<year<<setw(8)<<dateYr<<setw(12)<<setprecision(2)<<fv<<endl;
+
+    return 0;
+}
---
+++ Class/Savings_WhileLoop/main.cpp
@@ -0,0 +1,49 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on June , 2016,  PM
+ * Purpose: Savings with while loop
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+#include <iomanip>
+using namespace std;  //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+    //Variables
+    float pv, fv, iRate;
+    int nYears;
+    
+    //Input Data
+    cout<<"Input the present value in $'s, interest rate in %, number of years in (yrs)"<<endl;
+    cin>>pv>>iRate>>nYears;
+    
+    //Process Data
+    iRate/=100.0f;//Convert to decimal 
+    fv=pv;        //Initialize the future value with the present
+    
+    //Loop to Display the yearly result
+    cout<<fixed;
+    cout<<"Table Produced with "<<setprecision(2)<<iRate*100<<"% interest\n";
+    cout<<"Year     Date    Savings $\n";
+    int year=0, dateYr=2016;
+    while(year<=nYears){
+        cout<<setw(4)<<year<<setw(8)<<dateYr<<setw(12)<<setprecision(2)<<fv<<endl;
+        fv*=(1+iRate);//pay each year some interest
+        int ifv=fv*100;
+        fv=ifv/100.0f;//Truncating to nearest penny
+        year++;
+        dateYr++;
+    }
+
+    return 0;
+}
