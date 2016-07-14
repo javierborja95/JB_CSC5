f21243b5539a2fea4771595b12f3e227c8ed98a2
javierborja95 <javierborja95@gmail.com>
2016-07-14 13:52:51 -0700

    lab

--- Project/MidTerm/MidTermMenu/main.cpp
+++ Project/MidTerm/MidTermMenu/main.cpp
@@ -51,26 +51,137 @@ int main(int argc, char** argv) {
 //                                 Problem 1
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
-//Outputs a figure X
+//Outputs a figure X.
 //      Inputs - Argument list void
 //      Internal Input
 //            Number -> dimension Number
 //      outputs - Return Void
 //      Internal Output
-//            feet   -> dimension feet
+//            X      -> dimension X
 void prob1(){
     cout<<"You are entering Problem 1"<<endl;
+    //Variables
+    int num,//Number input
+      temp1,//Temporary values
+      temp2;
     
+    //Input Data
+    cout<<"Enter a number 1-50 : ";
+    cin>>num;
+    
+    //Process Data
+    temp1=num;
+    temp2=1;
+    
+    //Output Data
+    if(num%2==1){                         //Odd Check
+        for(int x=1;x<=num;x++){          //x=rows
+            for(int y=1;y<=num;y++){      //y=columns
+                if(x==y){                 //If row=column then output number
+                    cout<<temp1;
+                    temp1--;              //Next iteration will be 1 less
+                }
+                else if(((num+1)-x)==y){  //If opposite row=column then output 1
+                    cout<<temp2;          //Output 1
+                    temp2++;              //Next iteration will be 1 more
+                    if (temp2==num/2+1)   //Skip 1 number when it reaches midpoint
+                        temp2++;
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }
+    }else{                                //Else Even
+        for(int x=1;x<=num;x++){          //x=rows
+            for(int y=1;y<=num;y++){      //y=columns
+                if(x==y){                 //If row=column then output 1
+                    cout<<temp2;
+                    temp2++;              //Next iteration will be 1 more
+                }
+                else if(((num+1)-x)==y){  //If opposite row=column then output number
+                    cout<<temp1;          //Output number
+                    temp1--;              //Next iteration will me 1 less
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+    cout<<endl;
 }
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
 //                                 Problem 2
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
-//      Inputs - None
-//      outputs - The Menu
+//Outputs  4 digit numbers with stars.
+//      Inputs - Argument list void
+//      Internal Input
+//            4 digit -> dimension 4 digit number
+//      outputs - Return Void
+//      Internal Output
+//           Stars, ? -> dimension Stars, ?
 void prob2(){
     cout<<"You are entering Problem 2"<<endl;
+    //Variables
+    char num1,num2,num3,num4;
+    
+    //Input Data
+    cout<<"Enter a 4 character number and press enter.\n";
+    cin>>num1>>num2>>num3>>num4;
+    
+    //Process Data
+    
+    //Output Data
+    cout<<endl<<num4<<" ";
+    num4-=49;//Because character, I need to subtract 49 so I can make calculations
+    if (num4>=0&&num4<=9){
+        do{
+            num4-=1;
+            cout<<"*";
+        }while(num4>=0);
+    }else if (num4==-1){//Input blank instead of '*' or '?'
+        cout<<" ";
+    }else cout<<"?";
+    //End num4
+    cout<<endl<<num3<<" ";
+    num3-=49;//Because character, I need to subtract 49 so I can make calculations
+    if (num3>=0&&num3<=9){
+        do{
+            num3-=1;
+            cout<<"*";
+        }while(num3>=0);
+    }else if (num3==-1){//Input blank instead of '*' or '?'
+        cout<<" ";
+    }else cout<<"?";
+    //End num3
+    cout<<endl<<num2<<" ";
+    num2-=49;//Because character, I need to subtract 49 so I can make calculations
+    if (num2>=0&&num2<=9){
+        do{
+            num2-=1;
+            cout<<"*";        
+        }while(num2>=0);
+    }else if (num2==-1){//Input blank instead of '*' or '?'
+        cout<<" ";
+    }else cout<<"?";
+    //End num2
+    cout<<endl<<num1<<" ";
+    num1-=49;//Because character, I need to subtract 49 so I can make calculations
+    if (num1>=0&&num1<=9){
+        do{
+            num1-=1;
+            cout<<"*";
+        }while(num1>=0);
+    }else if (num1==-1){//Input blank instead of '*' or '?'
+        cout<<" ";
+    }else cout<<"?";
+    cout<<endl;
+    //End num1
 }
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
--- Project/MidTerm/MidTermMenu/nbproject/Makefile-variables.mk
+++ Project/MidTerm/MidTermMenu/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=midtermmenu
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/midtermmenu
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/midtermmenu
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=midtermmenu.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/midtermmenu.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/midtermmenu.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Release=midtermmenu
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/midtermmenu
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/midtermmenu
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=midtermmenu.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/midtermmenu.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/midtermmenu.tar
 #
 # include compiler specific variables
 #
