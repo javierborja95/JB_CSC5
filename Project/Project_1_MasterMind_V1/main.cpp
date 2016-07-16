5420a61fc56e9420f612bc7097e8c6691db38b98
javierborja95 <javierborja95@gmail.com>
2016-07-15 18:45:20 -0700

    project

---
+++ Project/Project_1_MasterMind_V1/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Javier Borja
+ * Created on July , 2016,  PM
+ * Purpose:
+ */
+
+//System Libraries
+#include <iostream>   //Input/ Output Stream Library
+using namespace std;  //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+
+int main(int argc, char** argv) {
+    //Variables
+ 
+    //Input Data
+
+    //Process Data
+                   
+    //Output Data
+
+    return 0;
+}
+
