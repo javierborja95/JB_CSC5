5d2dd2a69efc2c49f404f66c90a2480ce4cc344b
javierborja95 <javierborja95@gmail.com>
2016-07-27 17:32:52 -0700

    proj

---
+++ Project/Project_1_MasterMind_V7/Array.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Array.h
+ * Author: Javier B
+ *
+ * Created on July 27, 2016, 5:24 PM
+ */
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARRAY_H */
+
